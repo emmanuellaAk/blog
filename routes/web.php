7fbd3bd4d1be71a1153f1f0fdb62b8b05ac0b8ae
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-01 15:36:19 +0000

    Implemented filesystem class directory reading

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +15,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
+
+
 
 Route::get('post/{post}', function($slug) {
 
-    if(! file_exists($path =  __DIR__ . "/../resources/posts/{$slug}.html")) {
-        
-        return redirect('/');
-    }
-    
-    $post = cache()->remember("posts.{$slug}" , 1200 , fn() => file_get_contents($path));
+    $post = Post::find($slug);
 
-    return view('post', ['post' => $post]);
+      return view('post', [
+        
+        'post' => $post
     
+    ]);
 });
+
+
+
