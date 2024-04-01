d59f4181c1bdc41f8f135a773c3b68d440e45d7a
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-01 10:32:31 +0000

    set up routing for the blog posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+<body>
+    
+<article>
+
+    <?= $post; ?>
+    
+</article>
+
+ <a href="/">Go Back</a>
+</body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
 <body>
     
 <article>
-    <h1>My First Post</h1>
+    <h1 > <a href="/post/my-first-post">My First Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod justo eget purus posuere, id dictum ligula eleifend. Nullam consectetur justo eget velit fermentum, vel porta mi pharetra. Mauris eu lectus id nunc consequat congue. Integer feugiat ligula ac nisi tincidunt, quis ultricies lectus tristique. Donec lacinia sapien at nisi lobortis, vitae fermentum libero aliquet. Fusce ut tellus ut nisi fringilla vulputate. Sed pretium turpis id libero egestas, vel gravida ipsum fermentum. Suspendisse malesuada est a justo blandit vestibulum. Vivamus efficitur libero id risus finibus efficitur. Curabitur condimentum convallis bibendum. Sed posuere est nec est malesuada, at volutpat libero dapibus. Sed vel leo non leo gravida mattis. Phasellus eu nunc non nisi fringilla dictum.
@@ -12,7 +12,7 @@
 </article>
 
 <article>
-    <h1>My Second Post</h1>
+    <h1> <a href="/post/my-second-post">My Second Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod justo eget purus posuere, id dictum ligula eleifend. Nullam consectetur justo eget velit fermentum, vel porta mi pharetra. Mauris eu lectus id nunc consequat congue. Integer feugiat ligula ac nisi tincidunt, quis ultricies lectus tristique. Donec lacinia sapien at nisi lobortis, vitae fermentum libero aliquet. Fusce ut tellus ut nisi fringilla vulputate. Sed pretium turpis id libero egestas, vel gravida ipsum fermentum. Suspendisse malesuada est a justo blandit vestibulum. Vivamus efficitur libero id risus finibus efficitur. Curabitur condimentum convallis bibendum. Sed posuere est nec est malesuada, at volutpat libero dapibus. Sed vel leo non leo gravida mattis. Phasellus eu nunc non nisi fringilla dictum.
@@ -20,7 +20,7 @@
 </article>
 
 <article>
-    <h1>My Third Post</h1>
+    <h1 > <a href="/post/my-third-post">My Third Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus euismod justo eget purus posuere, id dictum ligula eleifend. Nullam consectetur justo eget velit fermentum, vel porta mi pharetra. Mauris eu lectus id nunc consequat congue. Integer feugiat ligula ac nisi tincidunt, quis ultricies lectus tristique. Donec lacinia sapien at nisi lobortis, vitae fermentum libero aliquet. Fusce ut tellus ut nisi fringilla vulputate. Sed pretium turpis id libero egestas, vel gravida ipsum fermentum. Suspendisse malesuada est a justo blandit vestibulum. Vivamus efficitur libero id risus finibus efficitur. Curabitur condimentum convallis bibendum. Sed posuere est nec est malesuada, at volutpat libero dapibus. Sed vel leo non leo gravida mattis. Phasellus eu nunc non nisi fringilla dictum.
