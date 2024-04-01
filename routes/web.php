4942ed0bdf82c50a0e9f6588768fd8ac2dfd14ea
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-01 12:46:30 +0000

    caching data

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,14 @@ Route::get('/', function () {
 });
 
 Route::get('post/{post}', function($slug) {
-    $path =   __DIR__."/../resources/posts/{$slug}.html";
 
-    if(! file_exists($path)) {
+    if(! file_exists($path =  __DIR__ . "/../resources/posts/{$slug}.html")) {
+        
         return redirect('/');
     }
+    
+    $post = cache()->remember("posts.{$slug}" , 1200 , fn() => file_get_contents($path));
 
-    $post = file_get_contents($path);
-
-    return view('post', [
-        'post' => $post
-    ]);
+    return view('post', ['post' => $post]);
     
 });
