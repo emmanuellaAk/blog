80cc49136b2ddd3bded1912abcb900cc2102e1bc
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-08 13:13:22 +0000

    modified categories dropdown

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 Route::get('/', function () {
 
   return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories'=> Category::all()
     ]);
 });
 
@@ -38,14 +39,17 @@ Route::get('posts/{post:slug}', function(Post $post) {
 Route::get('categories/{category:slug}', function(Category $category) {
 
     return view('posts', [
-       'posts' => $category->posts
+       'posts' => $category->posts,
+       'currentCategory'=> $category,
+       'categories'=> Category::all()
     ]);
 });
 
 
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
-       'posts' => $author->posts
+       'posts' => $author->posts,
+       'categories'=> Category::all()
     ]);
 });
 
