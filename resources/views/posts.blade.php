4b82f4990a899c2ca2838d9ec5b45efbef058852
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-08 04:30:49 +0000

    Divide HTML& CSS into Partial Files and Components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,22 @@
 <x-layout>    
-@foreach ($posts as $post)
+        @include ('_posts-header')
+
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card/>
+
+            <div class="lg:grid lg:grid-cols-2">
+                <x-post-card/>
+                <x-post-card/>
+            </div>
+
+            <div class="lg:grid lg:grid-cols-3">
+               <x-post-card/>
+               <x-post-card/>
+               <x-post-card/>
+           </div>
+
+        </main>
+<!-- @foreach ($posts as $post)
     <article>
 
        <h1>
@@ -19,5 +36,5 @@
        </div>
 
     </article>
-@endforeach
-</x-layout>
+@endforeach -->
+</x-layout> 
